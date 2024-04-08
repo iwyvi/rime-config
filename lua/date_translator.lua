f8d210aca2ab61a21f6e8918a479077b059de627
iwyvi <iwyvi@outlook.com>
2024-04-08 10:23:56 +0800

    fix: 调整日期输入

--- lua/date_translator.lua
+++ lua/date_translator.lua
@@ -1,5 +1,5 @@
 local function date_translator(input, seg)
-  if (input == "riqi") then
+  if (input == "vdate") then
       --- Candidate(type, start, end, text, comment)
       yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
       yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), ""))
@@ -8,7 +8,7 @@ local function date_translator(input, seg)
       -- yield(Candidate("date", seg.start, seg._end, os.date("%m-%d"), ""))
       -- yield(Candidate("date", seg.start, seg._end, os.date("%m-%d-%Y"), ""))
   end
-  if (input == "oijb") then
+  if (input == "vtime") then
       --- Candidate(type, start, end, text, comment)
       yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), ""))
       yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), ""))
@@ -20,14 +20,14 @@ local function date_translator(input, seg)
   --     yield(Candidate("datetime", seg.start, seg._end, os.date("%Y-%m-%dT%H:%M:%S+08:00"), ""))
   -- end
 
-  if (input == 'oijbao') then
+  if (input == 'vtimestamp') then
       yield(Candidate('timestamp', seg.start, seg._end, os.time(os.date('*t')) * 1000, ""))
   end
 
   -- @JiandanDream
   -- https://github.com/KyleBing/rime-wubi86-jidian/issues/54
 
-  if (input == "xdqi") then
+  if (input == "vweek") then
       local weakTab = {'日', '一', '二', '三', '四', '五', '六'}
       -- yield(Candidate("week", seg.start, seg._end, "周"..weakTab[tonumber(os.date("%w")+1)], ""))
       yield(Candidate("week", seg.start, seg._end, "星期"..weakTab[tonumber(os.date("%w")+1)], ""))
